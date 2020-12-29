33501c1f11f7ec3c3ba2c2b3545fd96f6e032e14
venkatande <venkatande@hotmail.com>
2020-12-28 21:19:27 -0500

    Discovery client added/commented

--- Student/src/main/java/ny/wfh/controller/StudentController.java
+++ Student/src/main/java/ny/wfh/controller/StudentController.java
@@ -3,6 +3,8 @@ package ny.wfh.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,6 +28,15 @@ public class StudentController {
 	private RestTemplate restTemplate;
 	@Autowired
 	private WebClient.Builder webClientBuilder;
+	
+	/*@Autowired
+	private DiscoveryClient discoveryClient;
+
+	@RequestMapping("/service-instances/{applicationName}")
+	public List<ServiceInstance> serviceInstancesByApplicationName(
+			@PathVariable String applicationName) {
+		return this.discoveryClient.getInstances(applicationName);
+	}*/
 	
 	@RequestMapping("/studentCourses/{studentId}")
 	public List<Course> getStudentCoursesById(@PathVariable String studentId)
